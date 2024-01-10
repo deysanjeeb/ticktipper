600e7c3292aea427d406af993471e4bb5f367f1e
Sanjeeb Dey <sanjeebdey1@gmail.com>
2024-01-10 15:35:46 -0800

    extracted td content

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from time import sleep
 from bs4 import BeautifulSoup 
+import csv
 
 service = Service(executable_path=r'/usr/bin/chromedriver')
 options = webdriver.ChromeOptions()
@@ -12,28 +13,28 @@ driver = webdriver.Chrome(service=service, options=options)
 driver.get("https://www.nirmalbang.com/nb-research/equity-technical-intraday-tips-and-calls.aspx")
 input_element = driver.find_element("xpath",'//*[@id="rch_txtmobile"]')
 input_element.send_keys('8828420553')
-# Print the page title (to verify successful navigation)
 link_element = driver.find_element("id",'checkresearchmobile')
 link_element.click()
 sleep(15)
 html = driver.page_source 
   
-# this renders the JS code and stores all 
-# of the information in static HTML code. 
-  
-# Now, we could simply apply bs4 to html variable 
+
 soup = BeautifulSoup(html, "html.parser") 
-print(soup.prettify())
+# print(soup.prettify())
 all_divs = soup.find('tbody', {'id' : 'technicalcallcontent'}) 
-print(all_divs)
-tds = all_divs.find_all('td') 
-print(tds)
+# print(all_divs)
+trs = all_divs.find_all('tr') 
+# print(tds)
 count = 0
-for td in tds : 
-    print(td.text) 
-    count = count + 1
-    if(count == 10) : 
-        break
-print(driver.title)
+tips=[]
+for tr in trs : 
+    tds = tr.find_all('td')
+    td_texts = [td.text for td in tds]
+    print(td_texts)
+    print()
+    tips.append(td_texts)) 
+    
+    
+# print(tips)
 sleep(10)
 driver.quit()
